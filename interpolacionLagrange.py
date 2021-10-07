d7fd07baebb885078b69e75e6d8c6a2ad5c1ae3a
Belén <belenortizfiocca@gmail.com>
2021-10-07 19:24:17 -0300

    Correcciones en comentarios

--- interpolacionLagrange.py
+++ interpolacionLagrange.py
@@ -1,33 +1,32 @@
 import sympy as sym
 import numpy as np
 
-#definimos los valores de x y f(x)
-xi = np.array([1,2,3])
-fi = np.array([10,15,30])
+# definimos los valores de x y f(x)
+xi = np.array([1, 2, 3])
+fi = np.array([10, 15, 30])
 
-n = len(xi)
-x = sym.Symbol("x") #variable simbolica
+x = sym.Symbol("x") # variable simbolica
 i = 0
 
-P = []
+terminos_l = [] # recopila todos los L's calculados
 
-for i in range(0,n):
-    Lnum = [] #se alamacenan los numeradores de la iteracion
-    Lden = [] #se alamacenan los denominadores de la iteracion
+for i in range(0, len(xi)):
+    numeradores_l = [] # se alamacenan los numeradores de la iteracion
+    denominadores_l = [] # se alamacenan los denominadores de la iteracion
 
-    for j in range(0,n):
+    for j in range(0, len(xi)):
         if i != j:
-            num = (x - xi[j])
-            Lnum.append(num)
-            den = (xi[i] - xi[j])
-            Lden.append(den)
+            numerador = (x - xi[j])
+            numeradores_l.append(numerador)
+            denominador = (xi[i] - xi[j])
+            denominadores_l.append(denominador)
 
-    numerador = " * ".join(map(str, Lnum)) #se unen los numeradores obtenidos para cada L
-    denominador = " * ".join(map(str, Lden)) #se unen los denominadores obtenidos para cada L
-    p = "( " + numerador + " / " + denominador + ")" + " * " +str(fi[i]) #forma de L -> num/den * f(x)
-    P.append(p)
+    numerador_str = " * ".join(map(str, numeradores_l)) #se unen los numeradores obtenidos para cada L
+    denominador_str = " * ".join(map(str, denominadores_l)) #se unen los denominadores obtenidos para cada L
+    p = "( " + numerador_str + " / " + denominador_str + ")" + " * " +str(fi[i]) #forma de L -> num/den * f(x)
+    terminos_l.append(p)
 
-polinomio = " + ".join(map(str, P)) #se unen los L obtenidos para formar el polinomio interpolante
+polinomio = " + ".join(map(str, terminos_l)) #se unen los L obtenidos para formar el polinomio interpolante
 print("El polinomio interpolante es: \n" + polinomio)
 
 
--- metodoBiseccion.py
+++ metodoBiseccion.py
@@ -1,14 +1,15 @@
 import math
 
 def f(x):
-    return (math.e)**(3*x)-4 #funcion de prueba
+    return (math.e)**(3*x)-4 # funcion de prueba
 
 def biseccion(a, b, err):
-    i = 0 #contador para las iteraciones
-    err = err * 100 #error ingresado y lo pasamos a %
-    e = ((b - a) / 2) * 100 #error calculado en base a los datos ingresados
+    i = 0
+    err = err * 100 # es el margen de error que se quiere mantener para la aproximacion
+    e = ((b - a) / 2) * 100 # permite controlar el error actual que se tiene para compararlo con err
 
-    while e > err: #hasta que el error calculado no sea menor al solicitado se va a iterar
+    # como se quiere llegar a un error menor al deseado se iterara hasta que esta condicion no se cumpla
+    while e > err:
         m = (a + b) / 2
         if f(a) * f(m) > 0:
             a = m
