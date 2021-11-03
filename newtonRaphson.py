2ecdf3ece298ede010da07f82d9c1a4dc355783c
Belén <belenortizfiocca@gmail.com>
2021-11-03 09:24:43 -0300

    modificaciones para pasar el codigo de numpy a sympy
    decidi utilizar la libreria de sympy porque tiene un metodo para calcular derivadas (antes el usuario debia calcularlas). Se me hizo mucho mas practico e interesante aplicarlo y para ello tuve que hacer algunas correcciones en el codigo (poner symbol, agregar el lambdify para despues pasar a numpy y poder operar, etc)

--- newtonRaphson.py
+++ newtonRaphson.py
@@ -1,9 +1,15 @@
-import numpy as np
-
-fx = lambda x: (1/np.exp(x))-x
-dfx = lambda x: -(np.exp(-x))-1
+import sympy as sp
 
 def newtonRaphson(x0, err):
+    # datos de la funcion a aplicar el metodo y su derivada
+    x = sp.Symbol("x")
+    f = (1 / sp.exp(x)) - x
+    df = sp.diff(f)
+
+    # convertimos la entrada en sympy a numpy para poder trabajar con ella
+    fx = sp.lambdify(x, f, "numpy")
+    dfx = sp.lambdify(x, df, "numpy")
+
     err = err*100
     x = x0-fx(x0)/dfx(x0)
     e = abs((x-x0)/x)*100
