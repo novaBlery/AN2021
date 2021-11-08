1abaf6df66deac75d7d8271689a01e8bb9300a92
Belén <belenortizfiocca@gmail.com>
2021-11-07 23:51:19 -0300

    metodo de de eliminacion de Gauss
    se el metodo descendete y ascendente

--- metodoGauss.py
+++ metodoGauss.py
@@ -6,30 +6,34 @@ matriz_ab = np.concatenate((matriz_a, matriz_b), axis=1) # Matriz aumentada
 print("Matriz aumentada " + "\n" + str(matriz_ab) + "\n")
 
 
-for iteracion_p in range(len(matriz_ab)):
+def proceso_descendente(matriz):
 
-    matriz_resultante = matriz_ab.copy()
+    for iteracion_p in range(len(matriz)):
 
-    # lo primero es definir el pivote, que tiene la forma matriz_a[p, p]
-    pivote = matriz_ab[iteracion_p, iteracion_p]
+        matriz_resultante = matriz.copy()
 
-    for i in range(iteracion_p, len(matriz_ab)):
-        for j in range(iteracion_p, len(matriz_ab) + 1):
+        # lo primero es definir el pivote, que tiene la forma matriz_a[p, p]
+        pivote = matriz[iteracion_p, iteracion_p]
 
-            if i == iteracion_p:
-                # una vez en la posicion del pivote, lo segundo es dividir a la fila entera por el pivote
-                for k in range(len(matriz_ab) + 1):
-                    if pivote == 0:
-                        continue
-                    matriz_resultante[i, k] = matriz_ab[i, k] / pivote
-                break
+        for i in range(iteracion_p, len(matriz)):
+            for j in range(iteracion_p, len(matriz) + 1):
 
-            else:
-                matriz_resultante[i, j] = matriz_ab[i, j] - (matriz_ab[i, iteracion_p] * (matriz_ab[iteracion_p, j] / pivote))
+                if i == iteracion_p:
+                    # una vez en la posicion del pivote, lo segundo es dividir a la fila entera por el pivote
+                    for k in range(len(matriz) + 1):
+                        if pivote == 0:
+                            continue
+                        matriz_resultante[i, k] = matriz[i, k] / pivote
+                    break
 
-    matriz_ab = matriz_resultante
+                else:
+                    matriz_resultante[i, j] = matriz[i, j] - (
+                                matriz[i, iteracion_p] * (matriz[iteracion_p, j] / pivote))
 
-print(matriz_ab)
+        matriz = matriz_resultante
+
+    print("Proceso descendente\n", matriz)
+    return matriz
 
 
 
